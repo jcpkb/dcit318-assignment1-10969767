c4b73bf5a799cf2f52343b8219fb4f6378ea262c
jcpkb <jcpbrown@st.ug.edu.gh>
2024-06-26 20:24:26 +0000

    Initial commit: Add GradeCalculator, TicketPriceCalculator, and TriangleTypeIdentifier projects

---
+++ ConsoleApp-TicketPriceCalculator/ConsoleApp-TicketPriceCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 65 || age <= 12)
+            {
+                Console.WriteLine("Ticket Price: GHC7");
+            }
+            else
+            {
+                Console.WriteLine("Ticket Price: GHC10");
+            }
+        }
+    }
+}
